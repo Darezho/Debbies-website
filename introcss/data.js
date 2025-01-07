8539cfb04c1ad21813ef8205b5068895df9536b2
Darezho <fagbewesadeborah2003@gmail.com>
2025-01-07 08:45:33 -0500

    Add files via upload

---
+++ introcss/data.js
@@ -0,0 +1,30 @@
+export const data = [
+    {
+        img: "thumbnail-1.jpg",
+        description:"Talking Tech and AI with Google CEO Sundar Pichai!",
+        username:"Marques Brownlee",
+        views:"3.4M",
+        time:"6 months ago",
+    },
+    {
+        img: "thumbnail-1.jpg",
+        description:"Talking Tech and AI with Google CEO Sundar Pichai!",
+        username:"Marques Brownlee",
+        views:"3.4M",
+        time:"6 months ago",
+    },
+    {
+        img: "thumbnail-1.jpg",
+        description:"Talking Tech and AI with Google CEO Sundar Pichai!",
+        username:"Marques Brownlee",
+        views:"3.4M",
+        time:"6 months ago",
+    },
+    {
+        img: "thumbnail-1.jpg",
+        description:"Talking Tech and AI with Google CEO Sundar Pichai!",
+        username:"Marques Brownlee",
+        views:"3.4M",
+        time:"6 months ago",
+    }
+ ]
